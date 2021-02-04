dd83ab36b395d3d0bd31b6f47114f50ae018e34d
Alexey Frolov <foritlearning@gmail.com>
2021-02-04 06:27:22 +0300

    implemented views and methods for displaying all products, added a product creation page

--- Areas/admin/Controllers/PagesController.cs
+++ Areas/admin/Controllers/PagesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MVC_Store.Models.Data;
+using MVC_Store.Models.ViewModels.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,84 @@ namespace MVC_Store.Areas.admin.Controllers
         // GET: admin/Pages
         public ActionResult Index()
         {
+            //Объявляем список для представления (PageVM)
+            List<PageVM> pageList;
+
+            //Инициализируем список (DB)
+            using(Db db = new Db())
+            {
+                pageList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Возвращаем список в представление
+            return View(pageList);
+        }
+
+        // GET: admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
             return View();
+        }
+
+        // POST: admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //Проверка модели на валидность
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                //Объявляем переменную для краткого описания (Slug)
+                string slug;
+
+                //Инициализируем класс PageDTO
+                PagesDTO dto = new PagesDTO();
+
+                //Присваиваем заголовок модели
+                dto.Title = model.Title.ToUpper();
+
+                //Проверяем, есть ли краткое описание, если нет, присваиваем его
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else 
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                //Убеждаемся в уникальности заголовка и описания
+                if (db.Pages.Any(x => x.Title == model.Title))
+                {
+                    ModelState.AddModelError("", "That title allready exist.");
+                    return View(model);
+                }
+                else if(db.Pages.Any(x => x.Slug == model.Slug))
+                {
+                    ModelState.AddModelError("", "That slug allready exist.");
+                    return View(model);
+                }
+                //Присваиваем оставшиеся значения модели
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                //Сохранем модель в базу данных
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            //Передаём сообщение через TempData
+            TempData["SM"] = "You have added a new page!";
+
+            //Переадресовываем пользователя на метод INDEX
+            return RedirectToAction("Index");
         }
     }
 }
